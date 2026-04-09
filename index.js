a344c75e754224c75846fc5b4c6eb54a7e23b1c1
kamran-8674 <connect4kamran@gmail.com>
2026-04-09 22:39:56 +0600

    added get api for singal service

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -40,6 +40,13 @@ async function run() {
       const result =await cursor.toArray()
       res.send(result) 
 
+    })
+
+    app.get('services/:id', async (req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await servicesCollection.findOne(query)
+      res.send(result)
     })
 
     app.post('/services', async(req,res)=>{
