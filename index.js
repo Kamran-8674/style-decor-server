7d4d17212174d2dd55edda43dcbc9bd91bcca5a2
kamran-8674 <connect4kamran@gmail.com>
2026-05-12 09:54:09 +0600

    for deployment

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const db = client.db("style_decor_db");
     const userCollection = db.collection("users");
